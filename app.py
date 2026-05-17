06f52677e6d74059c317869591ea3487473d713b
sagar <narutouzamai626@gmail.com>
2026-05-17 21:40:14 +0530

    Added mail timeout

--- app.py
+++ app.py
@@ -28,6 +28,7 @@ app.secret_key = os.getenv("FLASK_SECRET_KEY", "attendance_system")
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_TIMEOUT'] = 10
 app.config['MAIL_USERNAME'] = os.getenv("EMAIL_USER")
 app.config['MAIL_PASSWORD'] = os.getenv("EMAIL_PASS")
 
